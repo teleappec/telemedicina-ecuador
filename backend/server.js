bd935e60cd8d9bfbd369469f2251548ec154838c
teleapp <tazbgs009@hotmail.com>
2026-08-19 18:26:36 -0500

    Agregar logger de peticiones

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const cors = require('cors');
 const db = require('./database');
 
 const app = express();
+// Middleware para ver peticiones entrantes en Render
+app.use((req, res, next) => {
+  console.log(`📡 Petición recibida: ${req.method} ${req.url}`);
+  next();
+});
 app.use(cors());
 app.use(express.json());
 
